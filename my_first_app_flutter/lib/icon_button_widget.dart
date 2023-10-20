c8c64d0e7877ee922ac90a3eb6491bd3de24ee35
radf0001 <radf0001@ce.pucmm.edu.do>
2023-10-19 21:37:41 -0400

    Agregando efecto de fondo

--- my_first_app_flutter/lib/icon_button_widget.dart
+++ my_first_app_flutter/lib/icon_button_widget.dart
@@ -9,18 +9,26 @@ class IconButtonWidget extends StatefulWidget {
 
 class _IconButtonWidgetState extends State<IconButtonWidget> {
   double _speakerVol = 0.0;
+  Color _backgroundColor = Colors.red; // Color de fondo azul inicial
+
+  void _updateBackgroundColor() {
+    // Ajustar la opacidad del color de fondo según el valor de _speakerVol
+    double opacity = (_speakerVol / 100).clamp(0.0, 1.0); // Asegurarse de que esté en el rango 0-1
+    _backgroundColor = Colors.red.withOpacity(opacity);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.green,
         title: Text("Control de Volumen"),
         centerTitle: true,
       ),
       body: Center(
         child: Container(
           padding: EdgeInsets.all(50.0),
+          color: _backgroundColor, // Color de fondo con opacidad dinámica
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -29,11 +37,12 @@ class _IconButtonWidgetState extends State<IconButtonWidget> {
                 onChanged: (newValue) {
                   setState(() {
                     _speakerVol = newValue;
+                    _updateBackgroundColor(); // Actualizar el color de fondo
                   });
                 },
                 min: 0,
                 max: 100,
-                divisions: 10, // Puedes ajustar este valor
+                divisions: 10,
                 label: "Volumen",
               ),
               Row(
@@ -42,21 +51,9 @@ class _IconButtonWidgetState extends State<IconButtonWidget> {
                   IconButton(
                     onPressed: () {
                       setState(() {
-                        if (_speakerVol < 100) {
-                          _speakerVol += 10;
-                        }
-                      });
-                    },
-                    icon: Icon(Icons.volume_up),
-                    iconSize: 50.0,
-                    color: Colors.lightBlue,
-                    tooltip: "Subir volumen de a 10",
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      setState(() {
                         if (_speakerVol > 0) {
                           _speakerVol -= 10;
+                          _updateBackgroundColor(); // Actualizar el color de fondo
                         }
                       });
                     },
@@ -64,6 +61,20 @@ class _IconButtonWidgetState extends State<IconButtonWidget> {
                     iconSize: 50.0,
                     color: Colors.lightBlue,
                     tooltip: "Bajar volumen de a 10",
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      setState(() {
+                        if (_speakerVol < 100) {
+                          _speakerVol += 10;
+                          _updateBackgroundColor(); // Actualizar el color de fondo
+                        }
+                      });
+                    },
+                    icon: Icon(Icons.volume_up),
+                    iconSize: 50.0,
+                    color: Colors.lightBlue,
+                    tooltip: "Subir volumen de a 10",
                   ),
                 ],
               ),
